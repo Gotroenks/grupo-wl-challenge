16e9dfbe3651c2ae521ea011e91e72a4c47eca30
Gotroenks <isaacmoura_gerboni@hotmail.com>
2022-06-20 09:29:27 -0300

    Desafio Unidac - Grupo Wl

---
+++ src/main/java/com/example/challengegrupowl/ChallengeGrupoWlApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengegrupowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeGrupoWlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeGrupoWlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/challengegrupowl/ChallengeGrupoWlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengegrupowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeGrupoWlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
